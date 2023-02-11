6642a6f045986632ec3269349f2521a6a5fa836c
mkawsar <kawsar.diu888@gmail.com>
2023-02-11 22:13:40 +0600

    setup mongodb

--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ import logger from 'morgan';
 // MIddleware
 import {decode} from './middlewares/jwt.js';
 
+// Routes
+import indexRouter from './routes/index.js';
+import userRouter from './routes/user.js';
+
+// Mongo connection
+import './config/mongo.js';
+
 const app = express();
 const port = process.env.PORT || '3000';
 app.set('port', port);
@@ -14,7 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.status(200).json({
+        success: true,
+        'message' : 'Welcome to nodeplayground'
+    });
+});
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+
+/** catch 404 and forward to error handler */
+app.use('*', (req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: 'API endpoint doesnt exist'
+    });
 });
 
 /** Create HTTP server. */
---
+++ server/routes/user.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import userController from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router
+    .get('/', userController.onGetAllUsers)
+    .post('/create', userController.onCreateUser)
+    .get('/:id/details', userController.onGetUserById)
+    .delete('/delete/:id', userController.onDereleteUserById);
+
+export default router;
