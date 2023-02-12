34dbbea8fa3d2b8e1dc27b053ab9c8528081c7ad
mkawsar <kawsar.diu888@gmail.com>
2023-02-12 09:56:42 +0600

    created user model

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,10 @@
+// utils
+import makeValidation from '@withvoid/make-validation';
+import UserModel, { USER_TYPE } from '../models/user.js';
+
 export default {
-    onGetAllUsers: async (req, res) => {},
-    onGetUserById: async (req, res) => {},
-    onCreateUser: async (req, res) => {},
-    onDereleteUserById: async (req, res) => {},
+    onGetAllUsers: async (req, res) => { },
+    onGetUserById: async (req, res) => { },
+    onCreateUser: async (req, res) => { },
+    onDereleteUserById: async (req, res) => { },
 }
---
+++ server/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+
+export const USER_TYPE = {
+    CONSUMER: 'consumer',
+    SUPPORT: 'support'
+};
+
+const userSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            default: () => uuidv4().replace(/\-/g, "");
+        },
+        name: String,
+        email: String,
+        type: String,
+        password: String
+    },
+    {
+        timestamps: true,
+        collection: 'users'
+    }
+);
+
+export default mongoose.model('User', userSchema);
