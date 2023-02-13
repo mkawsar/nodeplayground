14112f12b88640b461c4eba2133fa3f6180815bb
mkawsar <kawsar.diu888@gmail.com>
2023-02-13 14:14:49 +0600

    added password salt

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import mongoose, { Schema } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
 
@@ -13,15 +14,43 @@ const userSchema = new mongoose.Schema(
             type: String,
             default: () => uuidv4().replace(/\-/g, "")
         },
-        name: String,
-        email: String,
+        name: {
+            type: String,
+            required: true,
+            min: 3,
+            max: 255
+        },
+        email: {
+            type: String,
+            required: true,
+            min: 3,
+            max: 255
+        },
         type: String,
-        password: String
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+            max: 1024
+        }
     },
     {
         timestamps: true,
         collection: 'users'
     }
 );
+
+userSchema.pre('save', async (next) => {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+        return next(error);
+    }
+});
 
 export default mongoose.model('User', userSchema);
