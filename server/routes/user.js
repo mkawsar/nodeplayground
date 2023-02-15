d22e82940c7487ed0038bbdd28b73ac7498cfe51
mkawsar <kawsar.diu888@gmail.com>
2023-02-15 18:26:24 +0600

    crated user and admin middleware

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import userController from '../controllers/user.controller.js';
+import {authMiddleware, isAdmin, isUser} from '../middlewares/auth.middleware.js'
 
 const router = express.Router();
 
 router
-    .get('/', userController.onGetAllUsers)
+    .get('/', authMiddleware, userController.onGetAllUsers)
     .post('/create', userController.onCreateUser)
     .get('/:id/details', userController.onGetUserById)
     .delete('/:id/delete', userController.onDereleteUserById)
