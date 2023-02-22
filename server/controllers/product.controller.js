9f62304cd1a342fad2e6e1a2c58f18cb1ed1440a
mkawsar <kawsar.diu888@gmail.com>
2023-02-22 10:54:20 +0600

    get product list with sorting query

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -5,10 +5,44 @@ import {createProductValidation} from '../validators/product.validator.js';
 export default {
     handleGetAllProduct: async (req, res) => {
         try {
+            // Filtering
+            const queryObj = { ...req.query };
+            const excludeFields = ['page', 'limit', 'sort', 'fields'];
+            excludeFields.forEach(el => delete queryObj[el]);
+            let queryStr = JSON.stringify(queryObj);
+            queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+            let query = Product.find(JSON.parse(queryStr));
+            if (req.query.sort) {
+                const sortBy = req.query.sort.split(',').join(' ');
+                query = query.sort(sortBy);
+            } else {
+                query = query.sort('-createdAt');
+            }
+
+            // Limiting
+            if (req.query.fields) {
+                const fields = req.query.fields.split(',').join(' ');
+                query = query.select(fields);
+            } else {
+                query = query.select('-__v');
+            }
+
+            // Pagination
             const { page = 1, limit = 10 } = req.query;
-            let products = await Product.find().skip((page - 1) * limit).limit(limit * 1).sort({ createdAt: '-1' }).exec();
-            const count = await Product.countDocuments();
-            return res.status(200).json({success: true, message: 'Get all products', data: products, totalPages: Math.ceil(count / limit), currentPage: page});
+            const skip = (page - 1) * limit;
+            query = query.skip(skip).limit(limit);
+            const ProductCount = await Product.countDocuments();
+
+            if (req.query.page) {
+                if (skip > ProductCount) {
+                    return res.status(404).json({ success: false, error: 'This page does not exists.' });
+                }
+            }
+            const product = await query;
+
+            return res.status(200).json({success: true, message: 'Get all products', data: product, pages: Math.ceil(ProductCount / limit), current: page});
+    
         } catch (err) {
             return res.status(500).json({ success: false, error: err?.message });
         }
